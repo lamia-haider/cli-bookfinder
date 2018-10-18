ce6919148b3a5bf58966a4cded2133be7fb975cc
Lamia Haider <lamia.haider@gmail.com>
2018-10-18 04:34:39 +0000

    Changed website to Amazon since it has genres linking to titles as opposed to just a long list of titles

--- lib/bookfind/scraper.rb
+++ lib/bookfind/scraper.rb
@@ -1,22 +1,31 @@
 require "nokogiri"
 require "pry"
 require "open-uri"
+require "rake"
 
 class Scraper
   
   @@all = []
   
   def self.getinfo
-    booklist = Nokogiri::HTML(open("https://litreactor.com/columns/storyville-3-essential-books-you-should-read-in-every-major-genre/"))
-  end
-  
-  def self.getgen
-    allgen = []
-    booklist.css("h2"). each do |gen| allgen << gen.text.tr("\n\t", "")
+    
+    booklist = Nokogiri::HTML(open("https://www.amazon.com/best-sellers-books-Amazon/zgbs/books/ref=zg_bs_unv_b_1_10134_2/136-1904286-8361318"))
+    booklist.css("div.a-fixed-left-grid-inner ul ul li").each do |genre|
+      genre.text.tr("\n","")
       binding.pry
     end
+
+
   end
 end
+  
+#   def self.getgen
+#     allgen = []
+#     booklist.css(".zg_browseRoot"). each do |gen| allgen << gen.text.tr("\n\t", "")
+#       binding.pry
+#     end
+#   end
+# end
   
   # def getgenre
   #   self.getinfo.css("h2")
