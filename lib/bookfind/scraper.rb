3c6d1368e82a41070c9d7a6a51b5fa8ecacf53c9
Lamia Haider <lamia.haider@gmail.com>
2018-10-26 00:36:02 +0000

    Scraper able to pick out author, title and url

--- lib/bookfind/scraper.rb
+++ lib/bookfind/scraper.rb
@@ -34,11 +34,15 @@ class Scraper
     booklist = Nokogiri::HTML(open("https://www.amazon.com/Best-Sellers-Books-Science-Fiction-Fantasy/zgbs/books/25/ref=zg_bs_nav_b_1_b"))
     booklist.css(".zg-item-immersion").each do |info| 
       title  = info.css(".a-link-normal div").text.tr("\n", " ").strip
-      author = info.css(".a-row a-size-small").text.tr("\n", " ").strip
+      author = info.css(".a-size-small span.a-color-base").text.tr("\n", " ").strip
       urlbase = info.css("a.a-link-normal").attribute("href") # will need to prepend www.amazon.com
-      
-      
+      url = "www.amazon.com" + urlbase.to_s
+      Books.new(title, author, url)
       binding.pry
     end
   end
 end
+    # doc ||= Nokogiri::HTML(open(self.url))
+    # date = doc.xpath(//*[@id="productDetailsTable"]/tbody/tr/td/div/ul/li[4]/text()).text
+    # binding.pry 
+
