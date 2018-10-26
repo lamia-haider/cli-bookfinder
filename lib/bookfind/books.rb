735fbeb0337edc4f6ac8fc750f541ff7576f6440
Lamia Haider <36172730+lamia-haider@users.noreply.github.com>
2018-10-25 22:15:01 -0400

    Minor changes to books.rb

--- lib/bookfind/books.rb
+++ lib/bookfind/books.rb
@@ -1,28 +1,29 @@
 class Bookfind::Books
   attr_accessor :title, :author, :date, :url, :summary
   @@all = []
-
-  def initialize(title, author)
-    @title = title
-    @author = author
+  
+  def self.var_from_main(info)
     urlbase = info.css("a.a-link-normal").attribute("href") # will need to prepend www.amazon.com
-    @url = "www.amazon.com" + urlbase.to_s
-
+    title = info.css(".a-link-normal div").text.tr("\n", " ").strip
+    url = "www.amazon.com" + urlbase.to_s
+    author = info.css(".a-size-small span.a-color-base").text.tr("\n", " ").strip
+    self.new(title, url, author)
+  end
+  
+  def initialize(title, url, author)
+    @title = title
+    @url = url
+    @author = author
     @@all << self
-     binding.pry
   end
 
   
   def indivpg
-    indivpg = Nokogiri::HTML(open(self.url))
+    @indivpg = Nokogiri::HTML(open(self.url))
   end
   
   def summary
     @summary = indivpg.css(".bookDescription_feature_div")
-  
   end
-  
-
-    
-  
+  binding.pry
 end
