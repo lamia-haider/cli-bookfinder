cddd345230a33711f3e24caae98c7d9539c966a2
Lamia Haider <lamia.haider@gmail.com>
2018-11-08 03:26:10 +0000

    Finished first versions of books.rb and scraper.rb

--- lib/bookfind/books.rb
+++ lib/bookfind/books.rb
@@ -1,30 +1,20 @@
 class Bookfind::Books
-  attr_accessor :title, :author, :date, :url, :summary
+  attr_accessor :title, :author, :date, :url, :summary, :pagenum, :genre
   @@all = []
   
-  def self.getinforoman
-    booklist = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/List_of_science_fiction_novels"))
-    booklist.css(".mw-parser-output ul li").each do |book|
+  def self.getpage(booklist)
+    booklist.css(".mw-parser-output ul li").each do |book| 
       titleau = book.text.split("by")
-      @title = titleau[0]
-      @author = titleau[1]
-      urlbase = book.css("a").attribute("href").text
-      @url = "https://en.wikipedia.org" + urlbase
-      title = @title
-      author = @author
-      url = @url
-      Bookfind::Books.new(title, author, url)
-      @indivpg = Nokogiri::HTML(open(url))
-      @summary = @indivpg.css("div.mw-parser-output p").text
-      @pubdate = @indivpg.css("div.mw-parser-output table tbody tr th div").text
+      title = titleau[0].strip
+      author = titleau[1]
+      urlbase = book.at_css("a").attribute("href") 
+      url = "https://en.wikipedia.org/" + urlbase.text
+      self.new(title, author, url)
       binding.pry
     end
   end
   
-
-
-  
-  def initialize(title, author, url)
+  def initialize(title=nil, author=nil, url=nil)
     @title = title
     @url = url
     @author = author
@@ -34,15 +24,26 @@ class Bookfind::Books
   def self.all
     @@all
   end
-
   
-  def indivpg
-    @indivpg = Nokogiri::HTML(open(self.url))
+  
+  def getbook
+    @page = Nokogiri::HTML(open(url))
+  end
+  
+  def date
+    @date = @page.css("tr td")[7].text.strip
+  end
+  
+  def pagenum
+    @page.css("tr td")[9].text.strip
+  end
+  
+  def genre 
+    @genre = @page.css("tr td")[5].text.strip
   end
   
   def summary
-    @summary = indivpg.css("div.mw-parser-output p").text
-
+    @summary = @page.css("div.mw-parser-output p").text
   end
-
 end
+
--- lib/bookfind/scraper.rb
+++ lib/bookfind/scraper.rb
@@ -9,57 +9,39 @@ class Bookfind::Scraper
   
   def self.getinforoman
     booklist = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/List_of_science_fiction_novels"))
-    booklist.css(".mw-parser-output ul li").each do |book| 
-      titleau = book.text.split("by")
-      @title = titleau[0]
-      @author = titleau[1]
-      urlbase = book.css("a").attribute("href") 
-      @url = "https://en.wikipedia.org/" + urlbase.text
-      title = @title
-      author = @author
-      url = @url
-      pubdate = @page.at('tr:contains("Publication date")').text.strip
-      @page.css("tr td")[8].text.strip
-   binding.pry
-    end
+    # booklist.css(".mw-parser-output ul li").each do |book| 
+    #   titleau = book.text.split("by")
+    #   title = titleau[0]
+    #   author = titleau[1]
+    Bookfind::Books.getpage(booklist)
  
-
-   # Bookfind::Books.new(title, author)
   end
   
+  def url
+    urlbase = book.css("a").attribute("href") 
+    @url = "https://en.wikipedia.org/" + urlbase.text
+  end
+ 
+
   def getpage
     @page = Nokogiri::HTML(open(url))
   end
   
+  def date
+    @date = @page.css("tr td")[7].text.strip
+  end
+  
+  def pagenum
+    @page.css("tr td")[9].text.strip
+  end
+  
+  def genre 
+    @genre = @page.css("tr td")[5].text.strip
+  end
+  
   def summary
     @summary = @page.css("div.mw-parser-output p").text
-    binding.pry
   end
-    #  @pubdate = @indivpg.css("div.mw-parser-output table tbody tr th div").text
-    
-
-
-
-  
-  def initialize(title, author, url)
-    @title = title
-    @url = url
-    @author = author
-    @@all << self
-  end
-  
-  def self.all
-    @@all
-  end
-
-  
-  def indivpg
-    @indivpg = Nokogiri::HTML(open(self.url))
-  end
-  
-  def summary
-    @summary = indivpg.css("div.mw-parser-output p").text
-
-  end
-
 end
+
+
