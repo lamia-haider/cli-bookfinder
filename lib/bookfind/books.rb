052fd2de14053507af14542e9e9391bb5ab4c406
Lamia Haider <lamia.haider@gmail.com>
2018-11-13 20:50:14 +0000

    seems to be a server side issue that prevents capture of href

--- lib/bookfind/books.rb
+++ lib/bookfind/books.rb
@@ -1,3 +1,7 @@
+require 'net/http'
+
+
+
 class Bookfind::Books
   attr_accessor :title, :author, :date, :url, :summary
   @@all = []
@@ -11,11 +15,28 @@ class Bookfind::Books
     title = titleau[0].strip
     author = titleau[1]
     #ubase = book.css("i a")#[0]["href"]
-    url = "https://en.wikipedia.org/wiki/#{title.gsub(" ", "_")}"
-    self.new(title, author, url)
-    
- #binding.pry
+    if url != "invalid"
+      self.new(title, author, url)
+      binding.pry
+      end
+    end
   end
+  
+  def url_str
+    url_str = "https://en.wikipedia.org/wiki/#{title.gsub(" ", "_")}"
+  end
+  
+  def checkurl(url_str)
+    url = URI.parse(url_str) rescue false
+    url.kind_of?(URI::HTTP) || url.kind_of?(URI::HTTPS)
+  end
+  
+  def url
+    if checkurl == "true"
+      url = url_str
+    else "invalid"
+    
+  
   
   def initialize(title=nil, author=nil, url=nil)
     @title = title
