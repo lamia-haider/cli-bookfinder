4cbe8e8d50e1ecbf99fedcf655a922ee3ae92cf2
Lamia Haider <lamia.haider@gmail.com>
2018-12-06 04:15:50 +0000

    final edits before refactoring

--- lib/bookfind/cli.rb
+++ lib/bookfind/cli.rb
@@ -1,7 +1,7 @@
 class Bookfind::CLI
 
   def intro
-    puts "Hello human. I am the Scientific Anecdotes and Stories Suggester or SASS.".yellow
+    puts "Hello human. I am the Scientific Anecdotes and Stories Suggester Interface or SASSI.".yellow
     puts "Are you here to expand your small mamallian brain with fictional accounts of space, time travel and other such complex concepts?".yellow
     puts "Please just state 'yes' or 'no', we don't want you to strain yourself.".yellow
     call
@@ -26,7 +26,7 @@ class Bookfind::CLI
   def start
     @bookcl = Bookfind::Books.all.sample
     @bookurl = @bookcl.url
-    if @bookurl = "Unavailable"
+    if @bookurl == "Unavailable"
       start
     else
       page = Nokogiri::HTML(open(@bookurl))
@@ -43,18 +43,19 @@ class Bookfind::CLI
     bookcl = @bookcl
     puts "Why not try".yellow
     puts "'#{bookcl.title}'"
-    puts "Interested? If you want more information type 'yes'. If you want a different recommendation type 'no'. Type 'exit' if you have decided that reading is too hard.".yellow
+    puts "Interested? If you want more information type 'yes'. If you want a different recommendation type 'no'."
+    puts "Type 'exit' if you have decided that reading is too hard.".yellow
     input = gets.strip
     if input == "no"
       start
     elsif input == "yes"
-      puts " . . . . . .. . . . . .. . . . . .. . . . . .. . . . . .. . . . . ."
+      puts " "
       puts "Author: #{bookcl.author}"
-      puts " . . . . . .. . . . . .. . . . . .. . . . . .. . . . . .. . . . . ."
+      puts" "
       puts "Date Published: #{bookcl.date}"
-      puts " . . . . . ."
+      puts " "
       puts "Synopsis: #{bookcl.summary[0..900]}..."
-      puts " . . . . . .. . . . . .. . . . . .. . . . . .. . . . . .. . . . . ."
+      puts " "
       puts "Please visit the provided URL if you'd like to read further."
       puts "Website for more Information: #{bookcl.url}"
       puts "★・・・・・・★・・・・・・★・・・・・・★"
